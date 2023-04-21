402b6e85b637c63adfcd9b9a620d04411648b346
Andrei Panikov <apanikov@c2r13s5.42yerevan.am>
2023-04-21 14:57:49 +0400

    level4

--- level4/sort_list.c
+++ level4/sort_list.c
@@ -1,18 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
+// #include <stdio.h>
+// #include <stdlib.h>
 
-typedef struct testlist test;
+// typedef struct testlist test;
 
-struct testlist
-{
-	int data;
-	test *next;
-};
+// struct testlist
+// {
+// 	int data;
+// 	test *next;
+// };
 
-int cmp(int a, int b)
-{
-	return (a <= b);
-}
+// int cmp(int a, int b)
+// {
+// 	return (a <= b);
+// }
 
 test	*sort_list(test* lst, int (*cmp)(int, int))
 {
@@ -38,47 +38,47 @@ test	*sort_list(test* lst, int (*cmp)(int, int))
 
 
 
-void print_data(test one)
-{
-	printf("%d\n", one.data);
-}
+// void print_data(test one)
+// {
+// 	printf("%d\n", one.data);
+// }
 
-int main()
-{
-	test *one;
+// int main()
+// {
+// 	test *one;
 
-	one = (test *)malloc(sizeof(test));
-	one->data = 12;
+// 	one = (test *)malloc(sizeof(test));
+// 	one->data = 12;
 
-	test *two;
+// 	test *two;
 
-	two = (test *)malloc(sizeof(test)); 
-	two->data = 10;
+// 	two = (test *)malloc(sizeof(test)); 
+// 	two->data = 10;
 
-	test *tre;
+// 	test *tre;
 
-	tre = (test *)malloc(sizeof(test)); 
-	tre->data = 15;
+// 	tre = (test *)malloc(sizeof(test)); 
+// 	tre->data = 15;
 
-	one->next = two;
-	two->next = tre;
-	tre->next = NULL;
+// 	one->next = two;
+// 	two->next = tre;
+// 	tre->next = NULL;
 
-	test *tmp;
+// 	test *tmp;
 
-	tmp = one;
+// 	tmp = one;
 
-	while(tmp != NULL)
-	{
-		print_data(*tmp);
-		tmp = tmp->next;
-	}
+// 	while(tmp != NULL)
+// 	{
+// 		print_data(*tmp);
+// 		tmp = tmp->next;
+// 	}
 	
-	tmp = one;
-	tmp = sort_list(tmp,(*cmp));
-	while(tmp != NULL)
-	{
-		print_data(*tmp);
-		tmp = tmp->next;
-	}        
-}
+// 	tmp = one;
+// 	tmp = sort_list(tmp,(*cmp));
+// 	while(tmp != NULL)
+// 	{
+// 		print_data(*tmp);
+// 		tmp = tmp->next;
+// 	}        
+// }
